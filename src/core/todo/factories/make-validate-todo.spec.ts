1a31d2d1b3a2933bbc6108cb1674f4d2392bf20c
luczod <kzod5611@gmail.com>
2025-10-03 21:47:31 -0300

    chore: create makeValidatedTodo (unit)

--- src/core/todo/factories/make-validate-todo.spec.ts
+++ src/core/todo/factories/make-validate-todo.spec.ts
@@ -1,5 +1,8 @@
 import { makeValidateTodo } from "./make-validate-todo";
+import * as validateTodoDescriptionMod from "../schemas/validate-todo-description";
 import * as sanitizeStrMod from "@/utils/sanitize-str";
+import * as makeNewTodoMod from "./make-new-todo";
+import { randomUUID } from "crypto";
 
 describe("makeValidatedTodo (unit)", () => {
   it("should call sanitizeStr function with correct value", () => {
@@ -16,7 +19,70 @@ describe("makeValidatedTodo (unit)", () => {
     expect(sanitizeStrSpy).toHaveBeenCalledExactlyOnceWith(description);
     expect(sanitizeStrSpy).toHaveBeenCalledTimes(1);
   });
-  it("should call validateTodoDescription with sanitizeStr return", () => {});
-  it("should call makeNewTodo if validateTodoDescription return success", () => {});
-  it("should return validateTodoDescription.error  if validation fail", () => {});
+
+  it("should call validateTodoDescription with sanitizeStr return", () => {
+    // ARRANGE
+    const cleanDescription = "abcdef";
+    const validateTodoDescriptionSpy = vi
+      .spyOn(validateTodoDescriptionMod, "validateTodoDescription")
+      .mockReturnValue({
+        success: true,
+        errors: [],
+      });
+
+    // ACT
+    makeValidateTodo(cleanDescription);
+
+    // ASSERT
+    expect(validateTodoDescriptionSpy).toHaveBeenCalledExactlyOnceWith(
+      cleanDescription
+    );
+  });
+
+  it("should call makeNewTodo if validateTodoDescription return success", () => {
+    // ARRANGE
+    const cleanDescription = "abcdef";
+    vi.spyOn(
+      validateTodoDescriptionMod,
+      "validateTodoDescription"
+    ).mockReturnValue({
+      success: true,
+      errors: [],
+    });
+
+    const makeNewTodoSpy = vi
+      .spyOn(makeNewTodoMod, "makeNewTodo")
+      .mockReturnValue({
+        id: randomUUID(),
+        description: cleanDescription,
+        createdAt: new Date().toISOString(),
+      });
+
+    // ACT
+    makeValidateTodo(cleanDescription);
+
+    // ASSERT
+    expect(makeNewTodoSpy).toHaveBeenCalledExactlyOnceWith(cleanDescription);
+  });
+
+  it("should return validateTodoDescription.error  if validation fail", () => {
+    // ARRANGE
+    const cleanDescription = "abc";
+    vi.spyOn(
+      validateTodoDescriptionMod,
+      "validateTodoDescription"
+    ).mockReturnValue({
+      success: false,
+      errors: ["Description must have more than 3 characters"],
+    });
+
+    // ACT
+    const newTodo = makeValidateTodo(cleanDescription);
+
+    // ASSERT
+    expect(newTodo).toStrictEqual({
+      success: false,
+      errors: ["Description must have more than 3 characters"],
+    });
+  });
 });
--- src/utils/sanitize-str.ts
+++ src/utils/sanitize-str.ts
@@ -1,5 +1,4 @@
 export function sanitizeStr(s: string): string {
-  console.log("Call original function", s);
   const cleaned = !s || typeof s !== "string" ? "" : s.trim().normalize();
   return cleaned;
 }
