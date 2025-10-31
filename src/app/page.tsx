5958abcdb822886ba5d8985ac799c039b3270e76
luczod <kzod5611@gmail.com>
2025-10-31 15:41:52 -0300

    getByRole, beforeEach and afterAll in Playwright with Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import { TodoContainer } from "@/components/TodoContainer";
+import { Metadata } from "next";
 
 export const dynamic = "force-dynamic"; // only in production
+
+export const metadata: Metadata = {
+  title: "Tests with Vitest and Playwright",
+};
 
 export default function Home() {
   return <TodoContainer />;
--- tests-e2e/home.e2e.ts
+++ tests-e2e/home.e2e.ts
@@ -1,13 +1,51 @@
-import { test, expect } from "@playwright/test";
+import { makeTestTodoRepository } from "@/core/__tests__/utils/make-test-todo-repository";
+import { test, expect, Page } from "@playwright/test";
+
+const HOME_URL = "/";
+const HEADING = "to-do list";
+const INPUT = "Task";
+const BUTTON = "Create Task";
+const BUTTON_BUSY = "Creating task...";
+const NEW_TODO_TEXT = "New Todo";
+
+const getHeading = (p: Page) => p.getByRole("heading", { name: HEADING });
+const getInput = (p: Page) => p.getByRole("textbox", { name: INPUT });
+const getBtn = (p: Page) => p.getByRole("button", { name: BUTTON });
+const getBtnBusy = (p: Page) => p.getByRole("button", { name: BUTTON_BUSY });
+
+const getAll = (p: Page) => ({
+  heading: getHeading(p),
+  input: getInput(p),
+  btn: getBtn(p),
+  btnBusy: getBtnBusy(p),
+});
+
+test.beforeEach(async ({ page }) => {
+  const { cleanDatabase } = await makeTestTodoRepository();
+  await cleanDatabase();
+
+  await page.goto(HOME_URL);
+});
+
+test.afterAll(async () => {
+  const { cleanDatabase } = await makeTestTodoRepository();
+  await cleanDatabase();
+});
 
 test.describe("<Home /> (E2E)", () => {
   // Rendering
   test.describe("Rendering", () => {
     test("It must have the correct HTML title.", async ({ page }) => {
-      await page.goto("/");
-
-      await page.waitForTimeout(5000);
+      await expect(page).toHaveTitle("Tests with Vitest and Playwright");
     });
+  });
+
+  test("It must render the header, input, and button to create TODOs.", async ({
+    page,
+  }) => {
+    await expect(getHeading(page)).toBeVisible();
+    await expect(getInput(page)).toBeVisible();
+    await expect(getBtn(page)).toBeVisible();
   });
 
   // Creation
